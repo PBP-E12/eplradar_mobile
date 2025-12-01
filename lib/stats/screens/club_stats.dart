cfa8278ce59bb73454114362cbc7057728107dd1
raihanheriandry <raihanmaulana.heriandry@gmail.com>
2025-12-01 17:45:03 +0700

    add stat initiate file

--- lib/stats/screens/club_stats.dart
+++ lib/stats/screens/club_stats.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Stats extends StatelessWidget {
-  const Stats({super.key});
+class ClubStats extends StatelessWidget {
+  const ClubStats({super.key});
 
   @override
   Widget build(BuildContext context) {
